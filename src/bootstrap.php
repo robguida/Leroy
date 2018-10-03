946b246245851efabab7d00c42a791879f24ccc1
Rob Guida <robguida4>
2018-10-03 06:23:17 -0400

    bootstrap.php: moved back to src, and changed test for DEVENV to check $_SERVER['USER']

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,7 +5,7 @@
  * Date: 6/3/18
  * Time: 6:06 PM
  */
-define('DEVENV', in_array($_SERVER['SERVER_NAME'], ['leroysbackside.development.com', 'dev.robguida.com']));
+define('DEVENV', 'dev' == $_SERVER['USER']);
 if (DEVENV) {
     error_reporting(E_ALL);
     session_start();
