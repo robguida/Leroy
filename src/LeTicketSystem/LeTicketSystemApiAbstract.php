1e860be3675fdabe2c453e2f87a3809ac7e3af38
Robert Guida <robguida4@gmail.com>
2019-12-27 17:11:00 -0500

    LeTicketSystemApiAbstract, LeTicketSystemApiAbstractTest - created test to make sure the flattening out of an array works properly.

--- src/LeTicketSystem/LeTicketSystemApiAbstract.php
+++ src/LeTicketSystem/LeTicketSystemApiAbstract.php
@@ -14,12 +14,45 @@ namespace Leroy\LeTicketSystem;
  */
 abstract class LeTicketSystemApiAbstract
 {
+    /** @var array */
+    protected $prefix_values = [];
+
     /**
      * @param array $arr
      * @return string
      */
-    protected function convertArrayIntoTicketBody(array $arr)
+    protected function convertDescriptionIntoTicketBody(array $arr)
     {
-        return implode("\n", $arr);
+        $description = [];
+        $this->flattenDescriptionToSingleDimensionArray($arr, $description);
+        $output = implode("\n", $description);
+        return $output;
+    }
+
+    /**
+     * @param array $arr
+     * @param array $description
+     * @param int $index
+     */
+    protected function flattenDescriptionToSingleDimensionArray(array $arr, array & $description, $index = 0)
+    {
+        if (0 == $index) {
+            $prefix = $this->prefix_values[0];
+        } else {
+            $prefix = implode('', array_fill(0, $index, $this->prefix_values[1]));
+        }
+        foreach ($arr as $key => $val) {
+            if (is_array($val)) {
+                if (!empty($val)) {
+                    $description[] = "{$prefix} {$key}";
+                    $new_index = $index + 1;
+                    $this->flattenDescriptionToSingleDimensionArray($val, $description, $new_index);
+                } else {
+                    $description[] = "{$prefix} {$key} = 'EMPTY'";
+                }
+            } else {
+                $description[] = "{$prefix} {$key} = {$val}";
+            }
+        }
     }
 }
--- test/UnitTests/LeTicketSystem/LeTicketSystemApiAbstractTest.php
+++ test/UnitTests/LeTicketSystem/LeTicketSystemApiAbstractTest.php
@@ -15,18 +15,126 @@ use Leroy\LeTicketSystem\LeTicketSystemApiAbstract;
 use Leroy\LeTicketSystem\LeTicketSystemRequestModel;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class LeTicketSystemApiAbstractTest
+ * @package LeroyTest\LeTicketSystem
+ */
 class LeTicketSystemApiAbstractTest extends TestCase
 {
     public function testConvertArrayIntoTicketBody()
     {
-
+        $model = new LeTicketSystemRequestModel();
+        $model->setDescription(
+            [
+                'Test 1' => [
+                    'Test 1.a' => 'String 1.a',
+                    'Test 1.b' => ['Test 1.b.1' => 'String 1.b.1', 'Test 1.b.2' => 'String 1.b.2'],
+                    'Test 1.c' => [
+                        'Test 1.c.1' => ['Test 1.c.1.a' => 'String 1.c.1.a', 'Test 1.c.1.b' => 'String 1.c.1.b'],
+                        'Test 1.c.2' => ['Test 1.c.2.a' => 'String 1.c.2.a', 'Test 1.c.2.b' => 'String 1.c.2.b'],
+                    ]
+                ],
+                'Test 2' => [
+                    'Test 2.a' => 'String 2.a',
+                    'Test 2.b' => 'String 2.b',
+                    'Test 2.c' => 'String 2.c',
+                    'Test 2.d' => ['Test 2.d.1' => 'String 2.d.1', 'Test 2.d.2' => 'String 2.d.2'],
+                    'Test 2.e' => [
+                        'Test 2.e.1' => ['Test 2.e.1.a' => 'String 2.e.1.a', 'Test 2.e.1.b' => 'String 2.e.1.b'],
+                        'Test 2.e.2' => ['Test 2.e.2.a' => 'String 2.e.2.a', 'Test 2.e.2.b' => 'String 2.e.2.b'],
+                        'Test 2.e.3' => ['Test 2.e.3.a' => 'String 2.e.3.a', 'Test 2.e.3.b' => 'String 2.e.3.b'],
+                    ]
+                ],
+                'Test 3' => [
+                    'Test 3.a' => 'String 3.a',
+                    'Test 3.b' => 'String 3.b',
+                    'Test 3.c' => 'String 3.c',
+                    'Test 3.d' => ['Test 3.d.1' => 'String 3.d.1', 'Test 3.d.2' => 'String 3.d.2'],
+                    'Test 3.e' => [
+                        'Test 3.e.1' => ['Test 3.e.1.a' => 'String 3.e.1.a', 'Test 3.e.1.b' => 'String 3.e.1.b'],
+                        'Test 3.e.2' => ['Test 3.e.2.a' => 'String 3.e.2.a', 'Test 3.e.2.b' => 'String 3.e.2.b'],
+                        'Test 3.e.3' => ['Test 3.e.3.a' => 'String 3.e.3.a', 'Test 3.e.3.b' => 'String 3.e.3.b'],
+                    ]
+                ]
+            ]
+        );
+        $testApi = new LeTicketSystemApi();
+        $result = $testApi->testConvertArrayIntoTicketBody($model);
+        $this->assertTrue(is_string($result));
+        $lines = explode("\n", $result);
+        $i = 0;
+        foreach ($lines as $line) {
+            $this->assertEquals($line, $this->array[$i++]);
+        }
     }
+
+    private $array = [
+        'h1. Test 1',
+        '* Test 1.a = String 1.a',
+        '* Test 1.b',
+        '** Test 1.b.1 = String 1.b.1',
+        '** Test 1.b.2 = String 1.b.2',
+        '* Test 1.c',
+        '** Test 1.c.1',
+        '*** Test 1.c.1.a = String 1.c.1.a',
+        '*** Test 1.c.1.b = String 1.c.1.b',
+        '** Test 1.c.2',
+        '*** Test 1.c.2.a = String 1.c.2.a',
+        '*** Test 1.c.2.b = String 1.c.2.b',
+        'h1. Test 2',
+        '* Test 2.a = String 2.a',
+        '* Test 2.b = String 2.b',
+        '* Test 2.c = String 2.c',
+        '* Test 2.d',
+        '** Test 2.d.1 = String 2.d.1',
+        '** Test 2.d.2 = String 2.d.2',
+        '* Test 2.e',
+        '** Test 2.e.1',
+        '*** Test 2.e.1.a = String 2.e.1.a',
+        '*** Test 2.e.1.b = String 2.e.1.b',
+        '** Test 2.e.2',
+        '*** Test 2.e.2.a = String 2.e.2.a',
+        '*** Test 2.e.2.b = String 2.e.2.b',
+        '** Test 2.e.3',
+        '*** Test 2.e.3.a = String 2.e.3.a',
+        '*** Test 2.e.3.b = String 2.e.3.b',
+        'h1. Test 3',
+        '* Test 3.a = String 3.a',
+        '* Test 3.b = String 3.b',
+        '* Test 3.c = String 3.c',
+        '* Test 3.d',
+        '** Test 3.d.1 = String 3.d.1',
+        '** Test 3.d.2 = String 3.d.2',
+        '* Test 3.e',
+        '** Test 3.e.1',
+        '*** Test 3.e.1.a = String 3.e.1.a',
+        '*** Test 3.e.1.b = String 3.e.1.b',
+        '** Test 3.e.2',
+        '*** Test 3.e.2.a = String 3.e.2.a',
+        '*** Test 3.e.2.b = String 3.e.2.b',
+        '** Test 3.e.3',
+        '*** Test 3.e.3.a = String 3.e.3.a',
+        '*** Test 3.e.3.b = String 3.e.3.b',
+    ];
 }
 
+/**
+ * Class LeTicketSystemApi
+ * @package LeroyTest\LeTicketSystem
+ */
 class LeTicketSystemApi extends LeTicketSystemApiAbstract
 {
+    public function __construct()
+    {
+        $this->prefix_values = ['h1.', '*'];
+    }
+
+    /**
+     * @param LeTicketSystemRequestModel $model
+     * @return string
+     */
     public function testConvertArrayIntoTicketBody(LeTicketSystemRequestModel $model)
     {
-
+        return parent::convertDescriptionIntoTicketBody($model->getDescription());
     }
 }
