8e32c065daedff6fbe6ec3b75172ecede6da3cb8
Robert Guida <robguida4@gmail.com>
2019-12-27 15:20:56 -0500

    LeTicketSystemFactoryTest - implemented unit test

--- test/UnitTests/LeTicketSystem/LeTicketSystemRequestModelTest.php
+++ test/UnitTests/LeTicketSystem/LeTicketSystemRequestModelTest.php
@@ -14,11 +14,29 @@ use LeroyTestLib\LeroyUnitTestAbstract;
 
 class LeTicketSystemRequestModelTest extends LeroyUnitTestAbstract
 {
-    public function testTicketSystemRequestModelInstantiation()
+    public function testTicketSystemRequestModelInstantiationWithDescriptionAsAnArray()
     {
         $model = new LeTicketSystemRequestModel();
         $title = 'Model Test 1';
         $description = ['key1' => 'test1', 'key2' => 'test2'];
+        $priority = 'high';
+        $bug = 'bug';
+        $this->assertInstanceOf('Leroy\LeTicketSystem\LeTicketSystemRequestModel', $model);
+        $model->setTitle('Model Test 1');
+        $model->setDescription($description);
+        $model->setPriority($priority);
+        $model->setTicketType($bug);
+        $this->assertEquals($title, $model->getTitle());
+        $this->assertEquals($description, $model->getDescription());
+        $this->assertEquals($priority, $model->getPriority());
+        $this->assertEquals($bug, $model->getTicketType());
+    }
+
+    public function testTicketSystemRequestModelInstantiationWithDescriptionAString()
+    {
+        $model = new LeTicketSystemRequestModel();
+        $title = 'Model Test 1';
+        $description = 'Description Test';
         $priority = 'high';
         $bug = 'bug';
         $this->assertInstanceOf('Leroy\LeTicketSystem\LeTicketSystemRequestModel', $model);
