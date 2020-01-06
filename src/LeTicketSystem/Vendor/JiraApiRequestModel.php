99ef9860c26f7d07f961b57e8b9191c36df93096
Robert Guida <robguida4@gmail.com>
2020-01-06 11:46:11 -0500

    JiraApi - added static function createBug($project, $summary, $description, $priority, $issue_type) to make it even easier to create tickets.

--- src/LeTicketSystem/Vendor/JiraApiRequestModel.php
+++ src/LeTicketSystem/Vendor/JiraApiRequestModel.php
@@ -17,6 +17,18 @@ class JiraApiRequestModel extends LeTicketSystemRequestModel
     /** @var string */
     private $path_to_credentials;
 
+    const ISSUE_TYPE_BUG = 'Bug';
+    const ISSUE_TYPE_EPIC = 'Epic';
+    const ISSUE_TYPE_STORY = 'Story';
+    const ISSUE_TYPE_SUB_TASK = 'Sub-task';
+    const ISSUE_TYPE_TASK = 'Task';
+
+    const PRIORITY_HIGHEST = 'Highest';
+    const PRIORITY_HIGH = 'High';
+    const PRIORITY_MEDIUM = 'Medium';
+    const PRIORITY_LOW = 'Low';
+    const PRIORITY_LOWEST = 'Lowest';
+
     /**
      * @return string
      */
