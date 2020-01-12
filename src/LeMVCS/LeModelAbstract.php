a9f636f435ff8eb1b1aee5426291e7648585aad5
Robert Guida <robguida4@gmail.com>
2020-01-11 20:10:37 -0500

    LeDbService::insert() - added comments to see what sql statement is created when dupe key is true

--- src/LeMVCS/LeModelAbstract.php
+++ src/LeMVCS/LeModelAbstract.php
@@ -372,6 +372,8 @@ abstract class LeModelAbstract
         $sql = "INSERT INTO `{$this->getTableName()}` (`" . implode('`, `', $cols) . "`) " .
             "VALUES (" . implode(', ', $needles) . ") " .
             "{$on_duplicate_key_clause};";
+        error_log( __FILE__ . ' ' . __LINE__ . ' $sql: ' . $sql);
+        error_log(__FILE__ . ' ' . __LINE__ . ' $bindings: ' . print_r($bindings, true));
         $this->dbResult = $db->execute($sql, $bindings);
         if ($this->dbResult->isSuccess()) {
             if (!$on_duplicate_key_clause) {
@@ -379,8 +381,6 @@ abstract class LeModelAbstract
                 $output = $this->dbResult->getLastInsertId();
                 $this->loadFromId($output, true);
             } else {
-                error_log( __FILE__ . ' ' . __LINE__ . ' $sql: ' . $sql);
-                error_log(__FILE__ . ' ' . __LINE__ . ' $bindings: ' . print_r($bindings, true));
                 /* When on duplicate key update is used there is no last insert id. We
                     select on the values used in the insert and then get the pkey and return that */
                 $sql = "SELECT * FROM `{$this->getTableName()}` WHERE " . implode(' = ? AND ', $cols) . ' = ?;';
